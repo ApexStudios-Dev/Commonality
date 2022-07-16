a6145903b8cead2224f520a66901a925082a0db5
ApexModder <robsonlawson3@gmail.com>
2022-07-16 15:35:54 +0100

    Add EntityTypeTags from Forge-PR-8442 (https://github.com/MinecraftForge/MinecraftForge/pull/8442) Bump mod version - v2.2.1

--- src/main/java/xyz/apex/forge/commonality/tags/EntityTags.java
+++ src/main/java/xyz/apex/forge/commonality/tags/EntityTags.java
@@ -26,6 +26,62 @@ public interface EntityTags
 		TagKey<EntityType<?>> FROG_FOOD = EntityTypeTags.FROG_FOOD;
 	}
 
+	interface Forge
+	{
+		// The following tags are copied directly from Forge PR #8442 (https://github.com/MinecraftForge/MinecraftForge/pull/8442)
+		// TODO: Reference instances from Forges `Tags.EntityTypes` class if / when PR gets merged
+		TagKey<EntityType<?>> AMPHIBIANS = forgeTag("amphibians");
+		TagKey<EntityType<?>> AQUATIC = forgeTag("aquatic");
+		TagKey<EntityType<?>> ARTHROPODS = forgeTag("arthropods");
+		TagKey<EntityType<?>> AVIANS = forgeTag("avians");
+		TagKey<EntityType<?>> AVIANS_FOWLS = forgeTag("avians/fowls");
+		TagKey<EntityType<?>> BOSSES = forgeTag("bosses");
+		TagKey<EntityType<?>> CEPHALOPODS = forgeTag("cephalopods");
+		TagKey<EntityType<?>> CREEPERS = forgeTag("creepers");
+		TagKey<EntityType<?>> DRAGONS = forgeTag("dragons");
+
+		/**
+		 * Mobs that are logically composed of a base element
+		 * If your mob is a combination of elements, feel free to add it to all appropriate tags
+		 */
+		TagKey<EntityType<?>> ELEMENTALS = forgeTag("elementals");
+		TagKey<EntityType<?>> ELEMENTALS_FIRE = forgeTag("elementals/fire");
+		TagKey<EntityType<?>> ELEMENTALS_ICE = forgeTag("elementals/ice");
+		/**
+		 * Mobs that are meant to be antagonistic to the player
+		 */
+		TagKey<EntityType<?>> ENEMIES = forgeTag("enemies");
+		/**
+		 * Mobs that have an associated item with the 'fish' tag
+		 */
+		TagKey<EntityType<?>> FISH = forgeTag("fish");
+		TagKey<EntityType<?>> FLYING = forgeTag("flying");
+		TagKey<EntityType<?>> LAND = forgeTag("land");
+
+		TagKey<EntityType<?>> MAMMALS = forgeTag("mammals");
+		TagKey<EntityType<?>> MAMMALS_BOVINES = forgeTag("mammals/bovines");
+		TagKey<EntityType<?>> MAMMALS_CAMELIDS = forgeTag("mammals/camelids");
+		TagKey<EntityType<?>> MAMMALS_CANIDS = forgeTag("mammals/canids");
+		TagKey<EntityType<?>> MAMMALS_CAPRINES = forgeTag("mammals/caprines");
+		TagKey<EntityType<?>> MAMMALS_EQUINES = forgeTag("mammals/equines");
+		TagKey<EntityType<?>> MAMMALS_FELIDS = forgeTag("mammals/felids");
+		TagKey<EntityType<?>> MAMMALS_SWINES = forgeTag("mammals/swines");
+		TagKey<EntityType<?>> MAMMALS_URSIDS = forgeTag("mammals/ursids");
+
+		/**
+		 * Mobs that simulate an intelligent, independent relationship of some sort with the player
+		 */
+		TagKey<EntityType<?>> NPC = forgeTag("npc");
+		TagKey<EntityType<?>> REPTILES = forgeTag("reptiles");
+		TagKey<EntityType<?>> UNDEAD = forgeTag("undead");
+		TagKey<EntityType<?>> UNDEAD_SKELETONS = forgeTag("undead/skeletons");
+		TagKey<EntityType<?>> UNDEAD_ZOMBIES = forgeTag("undead/zombies");
+		/**
+		 * Mobs that prefer to live in lava
+		 */
+		TagKey<EntityType<?>> VOLCANIC = forgeTag("volcanic");
+	}
+
 	interface Common
 	{
 		TagKey<EntityType<?>> CHICKENS = forgeTag("chickens");
